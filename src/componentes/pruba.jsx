685744156a9118509d96745b1da018f09372a3a7
Agustina01rosa <luisanarosa1405@gmail.com>
2024-08-06 20:04:09 -0300

    agregando lo de widi

---
+++ src/componentes/pruba.jsx
@@ -0,0 +1,16 @@
+
+
+export default function EditScreenInfo ({path}: {path: String}){
+
+    
+}
+
+const styles= StyleSheet.create({
+    getStartedConteiner: {
+        alingItems: 'center',
+        marginHorizontal: 50,
+    }
+}
+
+)
+
