412ea0d921c87d4fcf7912d4ba07c4f58efd444c
Agustina01rosa <luisanarosa1405@gmail.com>
2024-08-16 10:31:36 -0300

    Configuro el nodemodules y tsconfig.json

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,9 @@ import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
 
 import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
+import {useColorScheme} from '@/components/useColorScheme';
+
+
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
