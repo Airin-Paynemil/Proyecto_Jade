f5ab5d049bfe008cc0d994dd6827ea0819c51e7b
Agustina01rosa <luisanarosa1405@gmail.com>
2024-08-16 22:05:24 -0300

    modifique/arregue barra de busqueda

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,17 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Alert, FlatList, Text } from 'react-native';
-import ButtonCustom from '@/components/boton';
-import SearchBar from '@/components/SearchBar';
+
 
 const App: React.FC = () => {
-  const [filteredData, setFilteredData] = useState(data);
-
-  const handlePress = () => {
-    Alert.alert('Botón presionado!');
-  };
-
-  const handleSearch = (query: string) => {
-    if (query) {
-      const filtered = data.filter(item =>
-        item.name.toLowerCase().includes(query.toLowerCase())
-      );
-      setFilteredData(filtered);
-    } else {
-      setFilteredData(data);
-    }
-  };
+  
+  
 
   return (
     <View style={styles.container}>
       {/* Descomentar la línea a continuación si quieres usar el botón */}
       {/* <ButtonCustom title="Me gusta" onPress={handlePress} /> */}
-      <SearchBar onSearch={handleSearch} />
-      <FlatList
-        data={filteredData}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-      />
+      
+      
     </View>
   );
 };
@@ -48,12 +29,6 @@ const styles = StyleSheet.create({
   },
 });
 
-const data = [
-  { id: 1, name: 'Atraves de mi ventana' },
-  { id: 2, name: 'a dos metro de ti' },
-  { id: 3, name: 'bajo la misma estrella' },
-  { id: 4, name: 'culpa mia' },
-  { id: 5, name: 'culpa de nosotros' },
-];
+
 
 export default App;
