0277d0e9b4a1933ee62c8dee3c6410f98acf5c96
Airin-Paynemil <airin.paynemil.epet20@gmail.com>
2024-08-20 16:01:39 -0300

    ahora se pueden presionar los botones

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,15 +1,20 @@
-import { StyleSheet} from 'react-native';
+import { StyleSheet, Button, TouchableHighlight} from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Biblioteca</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View style={styles.separatorVertical} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <View style={styles.barraContainer}>
-        <Text style={styles.barra}>Favoritos</Text>
-        <Text style={styles.barra}>Me Gusta</Text>
+        <TouchableHighlight onPress={() => alert('aca van los favoritos')}>
+          <Text style={styles.barra}>Favoritos</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => alert('aca van los me gusta')}>
+          <Text style={styles.barra}>Me Gusta</Text>
+        </TouchableHighlight>
       </View>
     </View>
   );
@@ -21,22 +26,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     backgroundColor: 'ECECEC',
+    paddingTop: 20,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
+  separatorVertical: {
     marginVertical: 10,
     height: 1,
-    width: '80%',
+    textAlign: 'center',
   },
-   barraContainer:{
-       flexDirection:"row",
-       justifyContent:"space-around"
-   },
-   barra:{
-     flexBasis:'auto',
-     paddingVertical :10
-   }
+  barraContainer:{
+    backgroundColor: "rgba(89, 17, 81,0.8)",
+    flexDirection:"row",
+    width: '100%',
+    justifyContent:"space-around",
+    },
+  barra:{
+    fontSize: 15,
+    fontWeight: 'bold',
+    width: 100,
+    flexBasis:'auto',
+    paddingVertical :10,
+  }
 });
